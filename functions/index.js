22ff35aaf057b7e8d34c8e7b0379df4ba6b97613
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-11 12:00:59 +0000

    Deleted comments and first hello world function

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,6 @@ admin.initializeApp();
 
 const express = require("express");
 const app = express();
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world");
-});
 
 exports.getScreams = functions.https.onRequest((req, res) => {
   admin
