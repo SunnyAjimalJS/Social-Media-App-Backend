039fcfe793618c3ac68a81771dd99dc8209d3c12
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-12 12:03:44 +0000

    Installed Firebase and added config to index.js to start working on User Authentication

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,23 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+const config = {
+  apiKey: "AIzaSyClJdQl77TVrlDAvXjCfBOuvZqiFOBS_GI",
+  authDomain: "socialape-62ab3.firebaseapp.com",
+  databaseURL:
+    "https://socialape-62ab3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "socialape-62ab3",
+  storageBucket: "socialape-62ab3.appspot.com",
+  messagingSenderId: "167455888246",
+  appId: "1:167455888246:web:6457cc706eb8d9eac16057",
+  measurementId: "G-P76FMHSQER",
+};
+
 const express = require("express");
 const app = express();
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
 
 app.get("/screams", (req, res) => {
   admin
@@ -47,4 +62,4 @@ app.post("/scream", (req, res) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region("europe-west1").https.onRequest(app);
