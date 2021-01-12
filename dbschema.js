e60bbf23a669ad9148e554f6700e9c9737aba4e9
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-12 11:03:22 +0000

    Created a database schema file to show the data structure in the firebase database/cloud firestore

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this will be the scream body",
+      createdAt: "2021-01-11T13:17:06.650Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
