87984435212e883467a8124017c290beeb658c44
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-12 16:13:43 +0000

    Completed validation logic by persisting the data to firestore and tested - all working!

--- functions/index.js
+++ functions/index.js
@@ -68,6 +68,7 @@ app.post("/signup", (req, res) => {
   };
 
   // TODO: validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then((doc) => {
@@ -80,9 +81,20 @@ app.post("/signup", (req, res) => {
       }
     })
     .then((data) => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then((token) => {
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch((err) => {
