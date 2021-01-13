a3e857d2d1f563b356813569b65cbddbfa2321ec
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-13 13:44:27 +0000

    Completed validation for email field to check if it's empty and also a valid email using regEx

--- functions/index.js
+++ functions/index.js
@@ -58,6 +58,12 @@ app.post("/scream", (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 const isEmpty = (string) => {
   if (string.trim() === "") return true;
   else return false;
@@ -74,7 +80,11 @@ app.post("/signup", (req, res) => {
 
   let errors = {};
 
-  if(isEmpty(newUser.email))
+  if (isEmpty(newUser.email)) {
+    errors.email = "Email must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email";
+  }
 
   // TODO: validate data
   let token, userId;
