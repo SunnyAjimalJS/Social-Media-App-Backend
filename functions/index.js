4634c3d75a70d463b6ba2f1170a9ebd742055294
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-19 13:30:21 +0000

    Added a post route for adding user profile details and moved on to the handler

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,21 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+} = require("./handlers/users");
 
 // Screams Routes:
-app.get("/screams", getAllScreams); //GET data/all screams from firebase collection.
-app.post("/scream", FBAuth, postOneScream); //POST a scream/data to firebase collection with FBAuth middleware to check for an auth header.
+app.get("/screams", getAllScreams); // GET data/all screams from firebase collection
+app.post("/scream", FBAuth, postOneScream); // POST a scream/data to firebase collection with FBAuth middleware to check for an auth header
+app.post("/user/image", FBAuth, uploadImage); // POST route to upload a user image
+app.post("/user", FBAuth, addUserDetails); // POST route to add user profile details
 
 // Users Routes:
-app.post("/signup", signup); //Signup Route.
-app.post("/login", login); //Login Route.
-app.post("/user/image", FBAuth, uploadImage); //Uploading a user image route
+app.post("/signup", signup); //Signup Route
+app.post("/login", login); //Login Route
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
