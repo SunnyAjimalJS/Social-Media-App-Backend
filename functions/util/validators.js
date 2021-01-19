1427d8538a119fd4f4de6ac56758aea19ad667bd
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-19 14:37:46 +0000

    Added condition checks to check if the bio is empty and to check if a url provided by a user contains http and if it doesn't then we're adding it

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -50,4 +50,9 @@ exports.reduceUserDetails = (data) => {
   let userDetails = {};
 
   if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  if (!isEmpty(data.website.trim())) {
+    if (data.website.trim().substring(0, 4) !== "http") {
+      userDetails.website = `http://${data.website.trim()}`;
+    }
+  }
 };
