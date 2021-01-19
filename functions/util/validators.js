8aa15e7be1707b0343db9159f646cf29dec45f2e
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-19 14:46:26 +0000

    Completed the reduceUserDetails validator

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -53,6 +53,9 @@ exports.reduceUserDetails = (data) => {
   if (!isEmpty(data.website.trim())) {
     if (data.website.trim().substring(0, 4) !== "http") {
       userDetails.website = `http://${data.website.trim()}`;
-    }
+    } else userDetails.website = data.website;
   }
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+  return userDetails;
 };
