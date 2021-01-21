f7f14a9f94e9d45df6635366874be78ffaa617b3
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-21 14:09:25 +0000

    Started working on a get route to get and access user information once logged in

--- functions/index.js
+++ functions/index.js
@@ -11,14 +11,15 @@ const {
   addUserDetails,
 } = require("./handlers/users");
 
-// Screams Routes:
+// Scream Routes:
 app.get("/screams", getAllScreams); // GET data/all screams from firebase collection
 app.post("/scream", FBAuth, postOneScream); // POST a scream/data to firebase collection with FBAuth middleware to check for an auth header
 app.post("/user/image", FBAuth, uploadImage); // POST route to upload a user image
 app.post("/user", FBAuth, addUserDetails); // POST route to add user profile details
+app.get("/user", FBAuth, getAuthenticatedUser); // GET route to get and access user data once logged in
 
-// Users Routes:
-app.post("/signup", signup); //Signup Route
-app.post("/login", login); //Login Route
+// User Routes:
+app.post("/signup", signup); // Signup Route
+app.post("/login", login); // Login Route
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
