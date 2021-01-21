a4d74793fa0f64eb7f142e24616bcd081effc438
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-21 12:59:16 +0000

    Added db schema to represent a userDetails object which will be in the redux store

--- dbschema.js
+++ dbschema.js
@@ -23,3 +23,21 @@ let db = {
     },
   ],
 };
+
+const userDetails = {
+  // Redux Data
+  credentials: {
+    userId: "cOpC6RJLfialVbGEd22x6XUiYVw1",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2021-01-18T16:22:13.325Z",
+    imageUrl: "image/random/random",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "London, UK",
+  },
+  likes: [
+    { userHandle: "user", screamId: "EpcLRIYTmkpM0BMdAtKz" },
+    { userHandle: "user", screamId: "9iDAWdywkICq8K9mtHlB" },
+  ],
+};
