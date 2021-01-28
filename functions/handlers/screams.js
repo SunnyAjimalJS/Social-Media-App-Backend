b30737bd5f967e64a512a74fcd152ee0277990ba
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-01-28 13:35:19 +0000

    Completed route and handler for deleting a scream endpoint and tested in Postman - all working

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -76,6 +76,30 @@ exports.getScream = (req, res) => {
     });
 };
 
+// Delete a scream:
+exports.deleteScream = (req, res) => {
+  const document = db.doc(`/screams/${req.params.screamId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Scream not found" });
+      }
+      if (doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: "Unauthorized" });
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.json({ message: "Scream deleted successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
 // Comment on a scream:
 exports.commentOnScream = (req, res) => {
   if (req.body.body.trim() === "")
--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const {
   getAllScreams,
   postOneScream,
   getScream,
+  deleteScream,
   commentOnScream,
   likeScream,
   unlikeScream,
@@ -22,17 +23,16 @@ const {
 app.get("/screams", getAllScreams); // GET data/all screams
 app.post("/scream", FBAuth, postOneScream); // POST route to create a scream
 app.get("/scream/:screamId", getScream); // GET route to get data from one scream
-app.delete('/scream/:screamId') // DELETE route to delete a scream 
+app.delete("/scream/:screamId", FBAuth, deleteScream); // DELETE route to delete a scream
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream); // POST route to create a comment on a scream
 app.get("/scream/:screamId/like", FBAuth, likeScream); // GET route to like a scream
 app.get("/scream/:screamId/unlike", FBAuth, unlikeScream); // GET route to unline a scream
-
 
 // User Routes for Firebase:
 app.post("/signup", signup); // POST route to provide login data and Signup
 app.post("/login", login); // POST route to login using user credentials and obtain an Auth Token for FBAuth (Firebase Auth) middleware
 app.post("/user/image", FBAuth, uploadImage); // POST route to upload a user image
 app.post("/user", FBAuth, addUserDetails); // POST route to add user profile details
-app.get("/user", FBAuth, getAuthenticatedUser); // GET route to get and access user data once logged in 
+app.get("/user", FBAuth, getAuthenticatedUser); // GET route to get and access user data once logged in
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
