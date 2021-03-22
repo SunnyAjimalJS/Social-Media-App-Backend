565c61094554fd4df84b4e31c1bf43f5bb372e88
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-03-22 11:15:07 +0000

    Enabled cors

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/fbAuth"); // Firebase Auth middleware to check for an auth token
 
+const cors = require("cors");
+app.use(cors());
+
 const { db } = require("./util/admin");
 
 const {
